a9c5cb5a5d1a1c9ecb7739833f73102a96fa8083
Vishal <vishaltyagi9837@gmail.com>
2025-11-19 14:43:27 +0530

    fixed api bugs

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -11,10 +11,8 @@
 //   };
 // };
 
-const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-  };
+const asyncHandler = (requestHandler) => (req, res, next) => {
+  Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
 };
 
 export { asyncHandler };
